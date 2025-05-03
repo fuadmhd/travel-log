cd2654bd38f45a30784704bfce7fe8d7cafe2ed3
Fuad Mohammed <152621427+fuadmhd@users.noreply.github.com>
2025-05-03 20:13:04 +0300

    add landing page (#50)

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <NuxtLink to="/" class="btn btn-ghost navbar-start  text-xl">
+      Travel Log
+    </NuxtLink>
+    <div class="navbar-end">
+      <button class="btn">
+        Sing in With Github
+        <Icon name="tabler:brand-github" size="24" />
+      </button>
+    </div>
+  </div>
+</template>

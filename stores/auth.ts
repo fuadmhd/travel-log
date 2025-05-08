72ea6293a00a0bbac826a89f3172f006bbef3029
fuad mohammed <fuadmnur19@gmail.com>
2025-05-08 08:19:32 +0300

    bug

--- stores/auth.ts
+++ stores/auth.ts
@@ -8,7 +8,6 @@ export const useAuthStore = defineStore("useAuthStore", () => {
     loading.value = true;
     await authClient.signIn.social({
       provider: "github",
-      callbackURL: "/dashboard",
       errorCallbackURL: "/error",
     });
     loading.value = false;
